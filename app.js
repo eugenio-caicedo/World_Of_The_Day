7676a581900abe97c06ba64c9a6577f76d75076e
euge <eugeniohernandez17@gmail.com>
2016-03-03 11:36:15 -0430

    Ultima configuracion.

--- app.js
+++ app.js
@@ -1,25 +1,23 @@
 var http = require('http');
-var SlackClient = require('slack-client');
+var request = require('request');
+var SlackClient = require('@slack/client');
 var randomWords = require('random-words');
 var wordnet = require('wordnet');
 var capitalize = require('string-capitalize');
+var RTM_EVENTS = SlackClient.RTM_EVENTS;
 var CLIENT_EVENTS = SlackClient.CLIENT_EVENTS;
 var RTM_CLIENT_EVENTS = CLIENT_EVENTS.RTM;
-var token = process.env.SLACK_API_TOKEN || 'xoxp-2203644484-15987698708-23824165876-fe4f538ab5';
+var token = process.env.SLACK_API_TOKEN || '';
 var date_server_old = new Date();
 var first = true;
+var check = true;
+var logger_text = "";
 
 //1. Se crea el cliente (tanto web como rtm)
 var slackClientRtm = new SlackClient.RtmClient(token);//{logLevel: 'debug'}
 var slackClientWeb = new SlackClient.WebClient(token);
-	
-//2. Creamos el evento para la autenticacion
-//slackClientRtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData){});
 
-//3. you need to wait for the client to fully connect before you can send messages
-//RTM_CLIENT_EVENTS.RTM_CONNECTION_OPENED   //'message'
-console.log(RTM_CLIENT_EVENTS);
-
+	//Funcion para buscar la palabra en el diccionario
 function findRandomWord(functionEnd){
 	//4. Buscamos la palabra y su definicion de la palabra
 	var word = randomWords({ exactly: 1 })[0];
@@ -34,7 +32,7 @@ function findRandomWord(functionEnd){
 			
 			date_server_old = new Date();
 			first = false;
-			
+			check = true;
 			functionEnd(word, text);
 		}
 		else
@@ -42,28 +40,45 @@ function findRandomWord(functionEnd){
 	});
 }
 
-slackClientRtm.on(RTM_CLIENT_EVENTS.RTM_CONNECTION_OPENED, function (message) {
-	if(/*message.channel.id === '' &&*/ date_server_old < new Date() || first){
-		
+function addHoursDate(date){
+	var copiedDate = new Date(date.getTime());
+	copiedDate.setHours(date.getHours() - 13);
+	return copiedDate;
+}
+
+//2. Evento para cuando se conecte al servidor
+slackClientRtm.on(RTM_CLIENT_EVENTS.RTM_CONNECTION_OPENED, function () {
+	//console.log("Conectado");
+});
+
+//3. Evento que se ejecuta cada vez que se envia un mensaje al servidor (concurrencia)
+slackClientRtm.on(RTM_CLIENT_EVENTS.RAW_MESSAGE, function (message) {
+	logger_text += ""+date_server_old+" '-' "+new Date()+" '-' "+message+" <br/>";
+	if(((addHoursDate(new Date()).getDate() - addHoursDate(date_server_old).getDate()) == 1 || first) && check){
+		check = false;
 		findRandomWord(function(word_of_the_day, definition){
 			//5. This will send the message 'this is a test message' to the channel identified by id 'C0CT96Q1Z' #id general = C025ZJYEE
-			/*slackClientWeb.chat.postMessage('C0CT96Q1Z', null, {
+			slackClientWeb.chat.postMessage('C025ZJYEE', null, {
 				username: 'Word Of The Day',
+				icon_emoji: ":rocket:",
 				attachments: JSON.stringify([{
 					title: capitalize(word_of_the_day),
 					text: definition,
 					color: "danger"
 				}])
-			});*/
+			});
 		});
 		
 	}
+	else {
+		request('https://world-of-the-day.herokuapp.com', function(error, response, body){
+			if(!error && response.statusCode == 200)
+				console.log(body);
+		});
+	}
 });
 
-slackClientRtm.on('error', function (error) {
-	console.error('Error:', error);
-});
-	
+//6. Se inicializa la conexion
 slackClientRtm.start();
 
 
@@ -73,7 +88,7 @@ const PORT = process.env.PORT || 3000;
 
 //We need a function which handles requests and send response
 function handleRequest(request, response){
-  var quote = "El servidor Esta Activo.";
+  var quote = logger_text;//"El servidor Esta Activo desde: "+date_server_old;
   response.end(quote);
 }
 
